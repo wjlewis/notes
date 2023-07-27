15d4bce995277c50ee8756476c439e1f0f2d77d5
William Lewis <wjlewis2@protonmail.com>
2023-07-27 08:37:55 -0400

    Save

--- compose.sh
+++ compose.sh
@@ -4,4 +4,5 @@ DIR=$(dirname $1)
 BASE=$(basename $1)
 OUT="${DIR}/${BASE%.*}.html"
 
+# Using local version of `tw` for now.
 npx nodemon --exec "tw < \"$1\" > \"$OUT\"" --watch "$1"
--- publish.sh
+++ publish.sh
@@ -4,8 +4,17 @@ echo "Cleaning dist"
 rm -rf dist
 mkdir dist
 
-echo "Copying artifacts to dist"
+echo "Copying notes into dist"
 find -type f \
      '(' -name "*.html" -o -name "*.css" ')' \
      -a -not -path "./dist/*" \
      -exec cp --parents '{}' ./dist ';'
+
+echo "Updating paths to main.css"
+find -type f \
+     -name "*.html" \
+     -path "./dist/*" \
+     -exec sed -i 's/href="\/main.css"/href="\/notes\/main.css"/' '{}' ';'
+
+echo "Publishing to GitHub pages"
+npx gh-pages@2.1.1 -d ./dist
